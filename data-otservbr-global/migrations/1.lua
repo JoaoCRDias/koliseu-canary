191734ab994f58909924a19762972328f523a39a
Joao Carlos <joaocrdias17@gmail.com>
2026-04-16 15:48:33 -0300

    fix migrations

--- data-otservbr-global/migrations/1.lua
+++ data-otservbr-global/migrations/1.lua
@@ -1,13 +1,57 @@
 function onUpdateDatabase()
-	logger.info("Updating database to version 1 (sample players)")
-	-- Rook Sample
-	db.query("UPDATE `players` SET `level` = 2, `vocation` = 0, `health` = 155, `healthmax` = 155, `experience` = 100, `soul` = 100, `lookbody` = 113, `lookfeet` = 115, `lookhead` = 95, `looklegs` = 39, `looktype` = 129, `mana` = 60, `manamax` = 60, `town_id` = 1, `cap` = 410 WHERE `id` = 1;")
-	-- Sorcerer Sample
-	db.query("UPDATE `players` SET `level` = 8, `vocation` = 1, `health` = 185, `healthmax` = 185, `experience` = 4200, `soul` = 100, `lookbody` = 113, `lookfeet` = 115, `lookhead` = 95, `looklegs` = 39, `looktype` = 129, `mana` = 90, `manamax` = 90, `town_id` = 8, `cap` = 470 WHERE `id` = 2;")
-	-- Druid Sample
-	db.query("UPDATE `players` SET `level` = 8, `vocation` = 2, `health` = 185, `healthmax` = 185, `experience` = 4200, `soul` = 100, `lookbody` = 113, `lookfeet` = 115, `lookhead` = 95, `looklegs` = 39, `looktype` = 129, `mana` = 90, `manamax` = 90, `town_id` = 8, `cap` = 470 WHERE `id` = 3;")
-	-- Paladin Sample
-	db.query("UPDATE `players` SET `level` = 8, `vocation` = 3, `health` = 185, `healthmax` = 185, `experience` = 4200, `soul` = 100, `lookbody` = 113, `lookfeet` = 115, `lookhead` = 95, `looklegs` = 39, `looktype` = 129, `mana` = 90, `manamax` = 90, `town_id` = 8, `cap` = 470 WHERE `id` = 4;")
-	-- Knight Sample
-	db.query("UPDATE `players` SET `level` = 8, `vocation` = 4, `health` = 185, `healthmax` = 185, `experience` = 4200, `soul` = 100, `lookbody` = 113, `lookfeet` = 115, `lookhead` = 95, `looklegs` = 39, `looktype` = 129, `mana` = 90, `manamax` = 90, `town_id` = 8, `cap` = 470 WHERE `id` = 5;")
+logger.info("Updating database to version 1 (winter update 2025 - task board)")
+
+	db.query([[
+		CREATE TABLE IF NOT EXISTS `player_bounty_tasks` (
+			`player_id` int NOT NULL,
+			`state` tinyint NOT NULL DEFAULT 0,
+			`difficulty` tinyint NOT NULL DEFAULT 0,
+			`bounty_points` int NOT NULL DEFAULT 0,
+			`reroll_tokens` tinyint NOT NULL DEFAULT 0,
+			`free_reroll` bigint NOT NULL DEFAULT 0,
+			`active_raceid` int NOT NULL DEFAULT 0,
+			`active_kills` int NOT NULL DEFAULT 0,
+			`active_required_kills` int NOT NULL DEFAULT 0,
+			`active_reward_exp` int NOT NULL DEFAULT 0,
+			`active_reward_points` tinyint NOT NULL DEFAULT 0,
+			`active_task_grade` tinyint NOT NULL DEFAULT 0,
+			`active_task_difficulty` tinyint NOT NULL DEFAULT 0,
+			`talisman_damage_level` tinyint NOT NULL DEFAULT 0,
+			`talisman_lifeleech_level` tinyint NOT NULL DEFAULT 0,
+			`talisman_loot_level` tinyint NOT NULL DEFAULT 0,
+			`talisman_bestiary_level` tinyint NOT NULL DEFAULT 0,
+			`preferred_lists` BLOB NULL,
+			`current_creatures_list` BLOB NULL,
+			CONSTRAINT `player_bounty_tasks_pk` PRIMARY KEY (`player_id`),
+			CONSTRAINT `player_bounty_tasks_players_fk`
+				FOREIGN KEY (`player_id`) REFERENCES `players` (`id`)
+				ON DELETE CASCADE
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+	]])
+
+	db.query([[
+		CREATE TABLE IF NOT EXISTS `player_weekly_tasks` (
+			`player_id` int NOT NULL,
+			`has_expansion` BOOLEAN NOT NULL DEFAULT FALSE,
+			`difficulty` tinyint NOT NULL DEFAULT 0,
+			`any_creature_total_kills` int NOT NULL DEFAULT 0,
+			`any_creature_current_kills` int NOT NULL DEFAULT 0,
+			`completed_kill_tasks` tinyint NOT NULL DEFAULT 0,
+			`completed_delivery_tasks` tinyint NOT NULL DEFAULT 0,
+			`kill_task_reward_exp` int NOT NULL DEFAULT 0,
+			`delivery_task_reward_exp` int NOT NULL DEFAULT 0,
+			`reward_hunting_points` int NOT NULL DEFAULT 0,
+			`reward_soulseals` int NOT NULL DEFAULT 0,
+			`soulseals_points` int NOT NULL DEFAULT 0,
+			`needs_reward` tinyint NOT NULL DEFAULT 0,
+			`kill_tasks` BLOB NULL,
+			`delivery_tasks` BLOB NULL,
+			CONSTRAINT `player_weekly_tasks_pk` PRIMARY KEY (`player_id`),
+			CONSTRAINT `player_weekly_tasks_players_fk`
+				FOREIGN KEY (`player_id`) REFERENCES `players` (`id`)
+				ON DELETE CASCADE
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+	]])
+
+	return true
 end
